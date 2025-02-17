c72631df2580b91f0673f172ceaa53331cae8766
Adrian Perez <adrian6@pdx.edu>
2025-02-16 21:21:35 -0800

    Setting up the project

---
+++ debug/deps/blog_website-1bb4677a3c899997.d
@@ -0,0 +1,5 @@
+/Users/adrianperez/blog_website/target/debug/deps/libblog_website-1bb4677a3c899997.rmeta: src/main.rs
+
+/Users/adrianperez/blog_website/target/debug/deps/blog_website-1bb4677a3c899997.d: src/main.rs
+
+src/main.rs:
---
+++ debug/deps/blog_website-d1aaa6cbf533b624.d
@@ -0,0 +1,5 @@
+/Users/adrianperez/blog_website/target/debug/deps/libblog_website-d1aaa6cbf533b624.rmeta: src/main.rs
+
+/Users/adrianperez/blog_website/target/debug/deps/blog_website-d1aaa6cbf533b624.d: src/main.rs
+
+src/main.rs:
