d8a4983a24eb984354f2a155fd33287b663b9433
Adrian Perez <adrian6@pdx.edu>
2025-02-28 00:14:18 -0800

    Going through the documentation

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # blog_website
 
-The project will be creating a website that resembles a blog.  
+The project will be creating a website that resembles a blog. 
+
+
+# Sources for the Project
+
+Axum Documentation: https://docs.rs/axum/latest/axum/
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+/* 02/28/25 - Reading the documentation to 
+
+get used to axum  */
+
+//calling the axum crate
+/*
+ And programming the example from 
+ the documentation: https://docs.rs/axum/latest/axum/
+
+*/ 
+use axum::{
+    routing::get,
+    Router,
+};
+
+#[tokio::main]
+async fn main() {
+
+    
+    //build the application with a single route
+    let app = Router::new().route("/", get(|| async {"Hello, World"} ));
+
+    //run the app on hyper 
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
