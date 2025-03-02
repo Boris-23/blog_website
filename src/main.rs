1bf7b7374c4dd33766cd3e5c432a7f9cbbb970a0
Adrian Perez <adrian6@pdx.edu>
2025-03-01 23:07:46 -0800

    Fixed tomol and added more axum stuff

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,39 @@ get used to axum  */
  And programming the example from 
  the documentation: https://docs.rs/axum/latest/axum/
 
+ https://docs.rs/axum/latest/axum/extract/index.html
+
 */ 
 use axum::{
-    routing::get,
+    extract::Json,
+    routing::{get,post},
+    handler::Handler,
     Router,
 };
+use serde::Deserialize;
 
+
+//https://docs.rs/axum/latest/axum/extract/index.html
+//instead of creating user, I'm using creating entry
+#[derive(Deserialize)]
+struct CreateEntry{
+
+    name: String,
+    title: String,
+    Date: String,
+    Comments: String,
+}
+
+async fn CreateEntry(Json(payload): Json<CreateEntry>){}
+
+//use serde::Deserialize;
 #[tokio::main]
 async fn main() {
 
     
     //build the application with a single route
-    let app = Router::new().route("/", get(|| async {"Hello, World"} ));
+    let app = Router::new().route("/", get(|| async {"Hello, World"} ))
+    .route("/users",post(CreateEntry));
 
     //run the app on hyper 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
